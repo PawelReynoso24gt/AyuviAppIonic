1634d032a862cfaed4d2de5624f22afa39ec16df
danipro777 <daniel1603@umes.edu.gt>
2025-01-14 22:42:23 -0600

    Instalando dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -56,8 +56,8 @@ const App: React.FC = () => {
             <PrivateRoute exact path="/registroComisiones" component={registroComisiones} />
             <PrivateRoute exact path="/registroMateriales" component={registroMateriales} />
             <PrivateRoute exact path="/registroActiviades" component={registroActividades} />
-            <PrivateRoute exact path="/registroAspirante" component={registroAspirante} />
-            <PrivateRoute exact path="/solicitudPendiente" component={solicitudPendiente} />
+            <Route exact path="/registroAspirante" component={registroAspirante} />
+            <Route exact path="/solicitudPendiente" component={solicitudPendiente} />
             <PrivateRoute exact path="/productosVoluntarios" component={productosVoluntarios} />
             <PrivateRoute exact path="/invitado" component={invitado} />
             <PrivateRoute exact path="/productosVoluntarios" component={productosVoluntarios} />
